860a3719ef957e898be2679197baaaa8bba7b2c3
Crystal <crystall.101600@gmail.com>
2025-03-23 21:41:42 +0530

    Used if else statement with a boolean.

---
+++ variables01.py
@@ -0,0 +1,7 @@
+name = 'John Smith'
+age = 20
+is_existing = False
+if not is_existing :
+    print(f'{name} is {age} years old and is a new patient')
+else :
+    print(f'{name} is an old patient')
