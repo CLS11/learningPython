f3fc4d54831d79058f26343bbb34a9fe42d0b15b
Crystal <crystall.101600@gmail.com>
2025-03-23 22:05:03 +0530

    Converted the string datatype into integer

---
+++ type_conversion.py
@@ -0,0 +1,6 @@
+birth_year = input('Enter your year of birth: ')
+print(type(birth_year)) #checking the datatype
+age = 2025 - int(birth_year) #type conversion of string into integer
+print(type(age))
+print(age)
+
