43aecc98b695f9f44816331ac69a7956699f62e8
Crystal <crystall.101600@gmail.com>
2025-03-25 00:06:07 +0530

    Exploring the methods in strings.

---
+++ string_methods.py
@@ -0,0 +1,7 @@
+message = 'Get lost'
+print(len(message)) #prints the length of the string
+print(message.upper()) #converts the string to uppercase
+print(message.lower()) #converts the string to lowercase
+print(message.find('G')) #Searching any character in the string
+print(message.replace('Get', 'Totally')) #replacing a part of string
+print('Get' in message) #bool value to check whether the word is present
