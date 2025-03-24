b5bc42125593a0aafa4f2b7b87b50dddcc8a133c
Crystal <crystall.101600@gmail.com>
2025-03-24 21:59:06 +0530

    Illustrated the use of formatted strings as an efficient approach.

---
+++ formatted_strings.py
@@ -0,0 +1,8 @@
+first = 'Shawn'
+last = 'Mendes'
+#Using string concatenation
+message = first + ' [' + last + '] is a musician.'
+print(message)
+#Using formatted string
+f_message = f'{first} [{last}] is a musician.'
+print(f_message)
