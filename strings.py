428a65d62c883880c449b563c1cbf2c5e35cea19
Crystal <crystall.101600@gmail.com>
2025-03-24 21:48:33 +0530

    Demontstration of using string in a different manner.

---
+++ strings.py
@@ -0,0 +1,7 @@
+course = 'Strings'
+print(course[0:-1])
+print(course[-1:-1]) #empty string
+print(course[1:-1]) #removed the 0th index character and the last index character
+
+#copying the entire variable
+another = course[:] #0:0 is assumed 
