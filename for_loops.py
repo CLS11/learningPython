a940b5f0a945e52cefbecd65e823e8b4a06b750f
Crystal <crystall.101600@gmail.com>
2025-03-25 15:37:58 +0530

    Example to understand basic for loops.

---
+++ for_loops.py
@@ -0,0 +1,6 @@
+for item in 'Bag':
+    print(item)
+for items in ['B1', 'B2', 'B3']:
+    print(items)
+for itemss in range(10): #range is in built function to store all the numbers
+    print(itemss)
