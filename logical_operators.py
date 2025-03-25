7837568cfe85ac636b49b13e5f8da1a9d01105a3
Crystal <crystall.101600@gmail.com>
2025-03-25 11:16:19 +0530

    Demonstrated the use of all 3 logical operators.

---
+++ logical_operators.py
@@ -0,0 +1,8 @@
+high_income = True
+good_credit = True
+criminal_record = False
+# if high_income and good_credit: LOGICAL AND
+if high_income or good_credit: #LOGICAL OR
+    print("Eligible for loan.")
+if good_credit and not criminal_record
+    print("Eligible for loan") #LOGICAL NOT
