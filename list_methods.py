061b2312b0ce30a4bc970eb80bdb4d48c6b83c0c
Crystal <crystall.101600@gmail.com>
2025-03-26 21:11:25 +0530

    Contains all the relevant methods for the list.

---
+++ list_methods.py
@@ -0,0 +1,12 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.append(44) #inserts the number at the last index
+print(numbers)
+numbers.insert(0, 10) #inserts the number at the beginning
+numbers.insert(-1, 34) #inserts the number at the end
+print(numbers)
+number.remove(2) #removes the item
+number.pop() #removes the last item
+print(50 in numbers) #checks the existence 
+print(numbers.count(4)) #counts the number of existence
+numbers.sort() #sorts the list in ascending order
+numbers.reverse() #sorts the list in descending order
