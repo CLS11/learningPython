fad1d95f889650cea1e1c8e28775f746db285bae
Crystal <crystall.101600@gmail.com>
2025-03-28 23:00:10 +0530

    Example to demonstrate the use of unpacking feature in python.

---
+++ unpacking.py
@@ -0,0 +1,10 @@
+#tuples
+coordinates = (1, 2, 3)
+#Using unpacking
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
+#lists
+coordinate = [1, 2, 3]
+#Using unpacking in the similar manner as tuples.
