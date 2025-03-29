a8b6542264ae0ebbf3a2190b0bd7e1d0da58f839
Crystal <crystall.101600@gmail.com>
2025-03-29 20:55:53 +0530

    Using a return statement to give the square of a number.

---
+++ functions02.py
@@ -0,0 +1,9 @@
+def square(number):
+     return number * number
+
+desired_number = input("Enter a number: ")
+result = square(int(desired_number))
+print(result)
+
+#print(square(3)) will give the same result
+
