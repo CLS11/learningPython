094a71d2832886b07699936b3b652337ec0a727f
Crystal <crystall.101600@gmail.com>
2025-03-30 00:39:05 +0530

    Illustrating class example.

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Point:
+    def move(self):
+        print("move")
+    
+    def draw(self):
+        print("draw")
+
+#Making an object
+point1 = Point()
+#Invoking the methods
+point1.draw()
+point1.move()
